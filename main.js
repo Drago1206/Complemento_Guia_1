669c0097e35007d4d6e0151c8017c686ca4ce323
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2022-09-14 07:12:20 -0500

    5

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 addEventListener("DOMContentLoaded",()=>{
-    let n1=0, n2=0,m=0
-    n1=Number(prompt("Ingrese el primer valor para calcular el area de un triangulo",4));
-    n2=Number(prompt("Ingresa el segundo valor para calcular el area del triangulo: ",3));
-    m=(n1*n2)/2
-    console.log(`El area del triangulo es: ${m}`)
+    let long=0, n1=0,n2=0
+    long=Number(prompt("Ingrese la longitud de la longitud de la circuferen"))
 })
